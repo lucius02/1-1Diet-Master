e1c5d6d551b520f767ee887a11e1c8fa19692f72
lucius02 <54938206+lucius02@users.noreply.github.com>
2021-09-14 10:24:57 +0200

    update 1.03

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.diet.app',
+  appName: '1:1 Diet',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
